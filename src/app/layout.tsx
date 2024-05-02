52a9e1fb94ee857822ff9866617cb59512e9dc64
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-05-02 18:10:11 +0200

    Application name and description as const

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Not a Dev | Inicio",
-    description: "Gestión y desarrolo integral de proyectos web & móviles",
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
 };
 
 export default function RootLayout({
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = "Webbi"
+export const APP_DESCRIPTION = "Tu conversión digital"
