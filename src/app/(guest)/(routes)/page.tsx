aed2ac3e5f7a7ea6364a87a1e832306a7f402aef
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-05-02 19:58:39 +0200

    Landing header with wave svg

--- src/app/(guest)/(routes)/page.tsx
+++ src/app/(guest)/(routes)/page.tsx
@@ -1,7 +1,21 @@
 import Navigator from "@/components/guest/navigator";
+import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants";
 
 export default function Page() {
     return <main className="flex flex-col items-center">
         <Navigator />
+
+        {/* @ Waves wallpaper */}
+        <img 
+            src="/assets/svg/waves.svg"
+            className="border border-red-500 fixed top-0 -z-10"
+            draggable="false"
+        />
+
+        <div className="w-2/3 mt-20">
+            <h1 className="text-8xl py-10 font-bold w-1/2">
+                { APP_NAME }, { APP_DESCRIPTION }
+            </h1>
+        </div>
     </main>
 }
--- src/components/guest/navigator.tsx
+++ src/components/guest/navigator.tsx
@@ -1,17 +1,22 @@
 import Icon from "../shared/icon";
+import { Button } from "@nextui-org/react";
 
 export default function Navigator() {
-    return <nav className="w-screen border-b flex justify-center items-center py-7 text-md">
+    return <nav className="w-screen border-b flex justify-center items-center py-5 text-md bg-white">
         <section className="flex items-center gap-10 w-1/3">
             <Icon />
         
             <ul className="flex items-center gap-5">
-                <li><a className="text-gray-400 hover:text-orange-500 transition-all" href="/">Home</a></li>
+                <li><a className="text-gray-400 hover:text-orange-500 transition-all" href="/">Servicios</a></li>
+                <li><a className="text-gray-400 hover:text-orange-500 transition-all" href="/">Clientes</a></li>
+                <li><a className="text-gray-400 hover:text-orange-500 transition-all" href="/">Equipo</a></li>
             </ul>
         </section>
 
         <section className="w-1/3 text-end"> 
-            Other content
+            <Button color="warning" variant="bordered">
+                Acceder
+            </Button>  
         </section>
     </nav>
 }
