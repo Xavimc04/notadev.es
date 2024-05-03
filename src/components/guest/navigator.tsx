80435080db5adbf048675659b35054c2aea520fa
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-05-03 16:26:46 +0200

    Nav items responsive design

--- src/components/guest/navigator.tsx
+++ src/components/guest/navigator.tsx
@@ -1,26 +1,35 @@
 import Icon from "../shared/icon";
 import { Button } from "@nextui-org/react";
+import SegmentOutlinedIcon from '@mui/icons-material/SegmentOutlined';
 
 export default function Navigator() {
     return <nav className="w-screen border-b flex justify-center items-center py-5 text-md bg-white px-5 lg:px-0">
         <section className="flex items-center gap-10 flex-1 lg:flex-none lg:w-1/2 xl:w-1/3">
             <Icon />
         
-            <ul className="flex items-center gap-5">
+            <ul className="items-center gap-5 hidden lg:flex">
                 <li><a className="text-gray-400 hover:text-orange-500 transition-all" href="/">Servicios</a></li>
                 <li><a className="text-gray-400 hover:text-orange-500 transition-all" href="/">Clientes</a></li>
                 <li><a className="text-gray-400 hover:text-orange-500 transition-all" href="/">Equipo</a></li>
             </ul>
         </section>
 
-        <section className="lg:w-1/3 text-end"> 
+        <section className="lg:w-1/3 text-end flex justify-end"> 
             <Button
                 color="warning" 
                 variant="bordered"
-                className="rounded-md"
+                className="rounded-md hidden lg:flex"
             >
                 Acceder
             </Button>  
+
+            <Button 
+                isIconOnly 
+                variant="bordered"
+                className="rounded-md lg:hidden"
+            >
+                <SegmentOutlinedIcon />
+            </Button>    
         </section>
     </nav>
 }
