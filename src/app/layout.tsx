95624f820310b94c43a10f1f06b41bab754047c4
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-05-08 15:30:01 +0200

    Favicon integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: APP_NAME,
     description: APP_DESCRIPTION,
+    icons: {
+        icon: "/assets/wevvi.png"
+    }
 };
 
 export default function RootLayout({
