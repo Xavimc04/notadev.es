c2cefb81d646d01ed13a53b1fc4d808f097acdca
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-05-08 15:55:22 +0200

    Newsletter form to single file

--- src/app/(guest)/(routes)/page.tsx
+++ src/app/(guest)/(routes)/page.tsx
@@ -1,9 +1,9 @@
 import Navigator from "@/components/guest/navigator";
 import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants";
 import { Button } from "@nextui-org/react";
-import MarkEmailUnreadOutlinedIcon from '@mui/icons-material/MarkEmailUnreadOutlined';
 import Differences from "@/components/layouts/landing/differences";
 import Articles from "@/components/layouts/landing/articles";
+import Newsletter from "@/components/layouts/landing/newsletter";
 
 export default function Page() {
     return <main className="flex flex-col items-center">
@@ -56,7 +56,7 @@ export default function Page() {
             </section>
 
             <img 
-                src="https://cupocode.com/wp-content/uploads/2022/09/web_d_gif.gif"
+                src="/assets/gif/portrait-website.gif"
                 className="hidden xl:block w-1/2 py-10"
                 draggable="false"
             />
@@ -102,30 +102,16 @@ export default function Page() {
         </section>
 
         {/* @ Newsletter */}
-        <section className="w-[80%] lg:w-2/3 border border-primary relative rounded-lg flex flex-col items-center gap-7 py-10 mb-20">
+        <section className="w-[80%] lg:w-2/3 border border-primary relative rounded-lg flex flex-col items-center gap-5 py-10 mb-20">
             <h2 className="font-extrabold text-4xl lg:text-5xl w-2/3 lg:w-full py-2 text-center overflow-hidden">
                 ¡Suscríbete a nuestro newsletter!
             </h2>
 
             <p className="lg:w-2/3 text-center px-10 lg:px-0 text-lg">
-                Recibe las últimas noticias y actualizaciones de nuestro equipo. 
+                Obtén consejos, tutoriales y recursos exclusivos directamente en tu bandeja de entrada. Únete a nuestra comunidad de apasionados por el desarrollo web y potencia tus habilidades. ¡No te pierdas nada en { APP_NAME }!
             </p>
 
-            <div className="flex items-center gap-3 w-[80%] md:w-auto">
-                <input 
-                    type="email"
-                    placeholder="Correo electrónico"
-                    className="py-3 px-4 border rounded-lg flex-1 md:flex-none md:w-80 focus:outline-none focus:border-transparent"
-                />
-
-                <Button
-                    size="lg"
-                    isIconOnly
-                    className="rounded-md bg-green-500 text-white flex items-center"
-                >
-                    <MarkEmailUnreadOutlinedIcon />
-                </Button>
-            </div>
+            <Newsletter />
 
             <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#fff_30%,#ffb500_100%)]"></div>
         </section>
